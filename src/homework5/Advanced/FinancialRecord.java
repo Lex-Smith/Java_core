292d46e200f0367d60abbed8540beee1043264ca
Lex-Smith <e.d.smith@mail.ru>
2022-12-16 01:28:22 +0300

    homework5, advanced task done

--- src/homework5/Advanced/FinancialRecord.java
+++ src/homework5/Advanced/FinancialRecord.java
@@ -1,2 +1,28 @@
-package homework5.Advanced;public class FinancialRecord {
+package homework5.Advanced;
+
+public class FinancialRecord {
+    private int incomes;
+    private int outcomes;
+
+
+    public FinancialRecord(int incomes, int outcomes) {
+        this.incomes = incomes;
+        this.outcomes = outcomes;
+    }
+
+    public int getIncomes() {
+        return incomes;
+    }
+
+    public void setIncomes(int incomes) {
+        this.incomes = incomes;
+    }
+
+    public int getOutcomes() {
+        return outcomes;
+    }
+
+    public void setOutcomes(int outcomes) {
+        this.outcomes = outcomes;
+    }
 }
--- src/homework5/Advanced/Task1.java
+++ src/homework5/Advanced/Task1.java
@@ -1,2 +1,62 @@
-package homework5.Advanced;public class Task1 {
+package homework5.Advanced;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class Task1 {
+
+    private static String incomes = "Incomes = ";
+    private static String outcomes = "Outcomes = ";
+    private static String separator = ", ";
+
+    public static void main(String[] args) throws IOException {
+
+        Random random = new Random(1);
+        FileWriter fileWriter = new FileWriter("src\\homework5\\Advanced\\report_2.txt");
+        String file = "src\\homework5\\Advanced\\report_2.txt";
+        String result = "";
+
+        for (int i = 0; i < 10; i++) {
+            FinancialRecord financialRecord = new FinancialRecord(random.nextInt(10000), random.nextInt(10000));
+            fileWriter.write(incomes + financialRecord.getIncomes() + separator + outcomes + financialRecord.getOutcomes() + "\n");
+        }
+        fileWriter.close();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        while (bufferedReader.ready()) {
+            String line = bufferedReader.readLine();
+            result += line + separator;
+        }
+        bufferedReader.close();
+
+        System.out.println("General Incomes = " + getGeneralIncomes(result) + separator + "General Outcomes = " + getGeneralOutcomes(result));
+    }
+
+    public static int getGeneralIncomes(String findAllIncomes) {
+        int generalIncomes = 0;
+
+        for (int i = 0; i < 10; i++) {
+            String inc = findAllIncomes.substring(findAllIncomes.indexOf(incomes) + 10, findAllIncomes.indexOf(separator + outcomes));
+            findAllIncomes = findAllIncomes.substring(findAllIncomes.indexOf(separator + outcomes) + 1);
+            generalIncomes += Integer.parseInt(inc);
+        }
+        return generalIncomes;
+    }
+
+    // Тут скорее всего надо было сделать через регулярные выражения но пока не разобрался как
+    public static int getGeneralOutcomes(String findAllOutcomes) {
+        int generalOutcomes = 0;
+        String cut = findAllOutcomes;
+
+        for (int i = 0; i < 10; i++) {
+            cut = cut.substring(cut.indexOf(outcomes));
+            String out = cut.substring(cut.indexOf(outcomes) + 11, cut.indexOf(separator));
+            cut = cut.substring(cut.indexOf(separator) + 1);
+            generalOutcomes += Integer.parseInt(out);
+        }
+        return generalOutcomes;
+    }
 }
